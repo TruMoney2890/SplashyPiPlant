181151349f099861066206c064f98f1c2f7e2c06
Christian Peters <christianpeters31@outlook.com>
2022-02-15 09:03:45 -0500

    Figured out how to write data to a text file from python flask. This is a core functionality of our program which we will now use to continue development. The next step in the process is to read the data we write to the file and display it on the site. We can do this with pop-ups or a host of other ways.

---
+++ TESTING/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, render_template
+from os import listdir
+app = Flask(__name__)
+
+@app.route('/')
+def my_form():
+   return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    input_nopol = request.form['text_box']
+    if request.method == 'POST':
+       with open('nopol.txt', 'a') as f:
+            f.write(str(input_nopol))
+    return render_template('index.html', nopol=input_nopol)
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
