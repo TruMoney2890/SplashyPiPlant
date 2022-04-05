ad602afe3e5b8d9cf75d42f23e8cb089883dedf3
Christian Peters <christianpeters31@outlook.com>
2022-04-05 12:34:12 -0400

    Stable verion in add plant. Adding data functionality. Most likely going to format data into a text file in order for program to read it. Also Corey Schaffer's python tutorial files in 'Tutorial'

--- TESTING/app.py
+++ TESTING/app.py
@@ -1,13 +1,13 @@
+## ADD PLANT LOCATION, AS IN WHAT LINE IT IS AT
+## ADD REMOVE FUNCTION 
+## CHECKBOXES FOR mL AMOUNTS
+## ADD ABILITY TO SELECT WHICH LINE THE PLANT IS AT AND DRAW A DIAGRAM 
+
 import json
 from flask import Flask, render_template, request, url_for, flash, redirect
 import urllib
 
 app = Flask(__name__)
-
-## ADD PLANT LOCATION, AS IN WHAT LINE IT IS AT
-## ADD REMOVE FUNCTION 
-## CHECKBOXES FOR mL AMOUNTS
-## ADD ABILITY TO SELECT WHICH LINE THE PLANT IS AT AND DRAW A DIAGRAM 
 
 app.config['SECRET_KEY'] = '15d95fea91abf12688dde77bf2ce14f98bd78533b71217e4'
 
@@ -17,21 +17,41 @@ messages = [{'title': 'Spring Cactus',
             'content': 'WATERED: 2 times a day'}
             ]
 
+
 @app.route('/')
 def index():
     return render_template('index.html', messages=messages)
+
 
 @app.route('/', methods=['POST'])
 def my_form_post():
     input_nopol = request.form['title']
     #input_nopol2 = request.form['content']
     if request.method == 'POST':
-       with open('OUTPUT.txt', 'w') as f:
+        with open('OUTPUT.txt', 'w') as f:
             f.write(str(input_nopol))
     return render_template('create.html', nopol=input_nopol)
 
+
 def main():
     return render_template('index.html')
+
+plantArt = {
+    "         __/)\ " + "\n" + 
+    "        .-(__(=:" + "\n" +
+    "        |    \)" + "\n" +
+    "  (\__  | " + "\n" +
+    " :=)__)-|  __/)" + "\n" +
+    "  (/    |-(__(=:" + "\n" + 
+    "______  |  _ \)" + "\n" +
+    "/     \ | / \ " + "\n" +
+    "    ___\|/___\ " + "\n" +
+    "   [         ]\ " + "\n" +
+    "    \       /  " + "\n" + 
+    "     \     / " + "\n" + 
+    "      \___/ "
+    
+}
 
 @app.route('/create/', methods=('GET', 'POST'))
 def create():
@@ -45,8 +65,8 @@ def create():
 
         # Need to figure out the source of each piece of text, and put it in the output file
         #with open('OUTPUT.txt', 'w') as f:
-         #   f.write(str(f'Title:{title}'))
-          #  f.write(str(f'Content:{content}'))
+        #  f.write(str(f'Title:{title}'))
+        #  f.write(str(f'Content:{content}'))
 
 
 
@@ -58,7 +78,8 @@ def create():
         # getting input with name = lname in HTML form 
         contentForm = request.form.get("content") 
 
-        plantInfo = 'WATERED: '+ water_amount+ ' mL EVERY: '+ water_frequency + ' hours' + '\n' + 'NOTES: ' + '\n' + water_notes
+        plantInfo = f'WATERED: {water_amount} mL EVERY: {water_frequency} hours' + '\n' + 'NOTES: ' + '\n' + water_notes
+
 
         if not title:
             flash('Title is required!')
@@ -74,33 +95,33 @@ def create():
                 f.write(str(f' Water Frequency: {water_frequency}\n'))
                 f.write(str(f' Notes: {water_notes}\n'))
 
-            flash('Added ' + title + ': ' + plantInfo)
+            flash(f'Added {title}: {plantInfo}')
             #flash(water_amount)
 
 
 
             # # Need to figure out how to format text inside of a JSON file
-            
+
             # # the file to be converted to 
             # # json format
             # filename = 'OUTPUT.txt'
-            
+
             # # dictionary where the lines from
             # # text will be stored
             # dict1 = {}
-            
+
             # # creating dictionary
             # with open(filename) as fh:
-            
+
             #     for line in fh:
             #         print(line)
-            
+
             #         # reads each line and trims of extra the spaces 
             #         # and gives only the valid words
             #         command, description = line.strip().split(None, 1)
-            
+
             #         dict1[command] = description.strip()
-            
+
             # # creating json file
             # # the JSON file is named as test1
             # out_file = open("test1.json", "w")
@@ -109,3 +130,8 @@ def create():
 
             return redirect(url_for('index'))
     return render_template('create.html')  
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+    
