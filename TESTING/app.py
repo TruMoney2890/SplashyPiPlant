9318aeae9f7e17687f09f6ff4ced7471291338d4
Christian Peters <christianpeters31@outlook.com>
2022-04-11 11:57:31 -0400

    website is hosted on custom domain. doing some cleaning. next task is the CSV analysis and the ability to edit posts

--- TESTING/app.py
+++ TESTING/app.py
@@ -56,28 +56,14 @@ def create():
 
     if request.method == 'POST':
         title = request.form['title']
-        # content = request.form['content']
         plantLocation = request.form['plantLocation']
         water_amount = request.form['water_amount']
         water_notes = request.form['water_notes']
 
-        # Need to figure out the source of each piece of text, and put it in the output file
-        #with open('OUTPUT.txt', 'w') as f:
-        #  f.write(str(f'Title:{title}'))
-        #  f.write(str(f'Content:{content}'))
-
-
-
         # url = 'http://127.0.0.1:5000/'
         # urllib.urlretrieve(url, fielname='webpage.html')
 
-        # getting input with name = fname in HTML form
-        titleForm = request.form.get("title")
-        # getting input with name = lname in HTML form 
-        contentForm = request.form.get("content") 
-
         plantInfo = f'WATERED: {water_amount} mL EVERY: 3 Hours' + '\n' + 'LOCATION: ' + plantLocation + '\n' + 'NOTES: ' + '\n' + water_notes
-
 
         if not title:
             flash('Title is required!')
@@ -95,40 +81,9 @@ def create():
             flash(f'Added {title}: {plantInfo}')
             #flash(water_amount)
 
-
-
-            # # Need to figure out how to format text inside of a JSON file
-
-            # # the file to be converted to 
-            # # json format
-            # filename = 'OUTPUT.txt'
-
-            # # dictionary where the lines from
-            # # text will be stored
-            # dict1 = {}
-
-            # # creating dictionary
-            # with open(filename) as fh:
-
-            #     for line in fh:
-            #         print(line)
-
-            #         # reads each line and trims of extra the spaces 
-            #         # and gives only the valid words
-            #         command, description = line.strip().split(None, 1)
-
-            #         dict1[command] = description.strip()
-
-            # # creating json file
-            # # the JSON file is named as test1
-            # out_file = open("test1.json", "w")
-            # json.dump(dict1, out_file, indent = 4, sort_keys = False)
-            # out_file.close()
-
             return redirect(url_for('index'))
     return render_template('create.html')  
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
-    
+    app.debug=True
+    app.run(host="0.0.0.0", port=5000)
